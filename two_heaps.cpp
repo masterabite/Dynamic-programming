9589940b93b744c8c99edb8cc42f610bf1a832c6
Артём <47632668+masterabite@users.noreply.github.com>
2022-12-11 00:34:57 +0300

    Update two_heaps.cpp

--- two_heaps.cpp
+++ two_heaps.cpp
@@ -1,34 +1,25 @@
-/*
-  дано множество камней с соответствующими весами.
-  программа находит минимальную разность весов двух 
-  куч, на которые можно разбить множество камней
-*/
-
 #include <iostream>
 
 using namespace std;
 
 int a[20];
-int n;
+int n, answer, all_sum;
 
-void rec(int* a, int i, int n, int current_sum, int& answer, int& all_sum) {
-    answer = min(answer, abs(all_sum - 2*current_sum));
+void rec(int i, int current_sum) {
+    answer = min(answer, abs(all_sum - 2 * current_sum));
     if (i == n) return;
-    rec(a, i + 1, n, current_sum + a[i], answer, all_sum);
-    rec(a, i + 1, n, current_sum, answer, all_sum);
+    rec(i + 1, current_sum + a[i]);
+    rec(i + 1, current_sum);
 }
 
 int main() {
-    int n;
     cin >> n;
-    int* a = new int[n];
-    int sum = 0;
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
-        sum += a[i];
+        all_sum += a[i];
     }
-    int answer = sum;
-    rec(a, 0, n, 0, answer, sum);
+    answer = all_sum;
+    rec(0, 0);
     cout << answer << '\n';
     return 0;
 }
